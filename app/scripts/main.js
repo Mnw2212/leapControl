fcee2628bdd781404672d053ebdde3c5a8481d8a
Mayuresh Waykole <mayuresh_waykole@external.mckinsey.com>
2016-05-19 19:32:05 +0530

    swipe works with app open in background

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,122 +1,111 @@
-var leapjs = require('leapjs');
-var robotjs = require('robotjs');
+'use strict';
+const leapjs = require('leapjs');
+const robotjs = require('robotjs');
 robotjs.setMouseDelay(0);
 
-function onSwipe(gesture){
-  // console.log("swipe worked");
-  var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-  //Classify as right-left
-  var swipeDirection;
-  if(isHorizontal){
-    if(gesture.direction[0] > 0){
-      console.log('right swipe');
-        swipeDirection = "right";
-    } else {
-        console.log('left swipe');
-        swipeDirection = "left";
+class leapControl {
+    constructor() {
+        // const pause = false;
     }
-  }
-  if(swipeDirection === "right"){
-    robotjs.keyTap('right');
-  }
-  else if (swipeDirection === 'left') {
-    robotjs.keyTap('left');
-  }
-  return 0;
+    static setPause() {
+        this.pause = true;
+        console.log(this.pause);
+    }
+
+    static unsetPause() {
+        this.pause = false;
+        console.log(this.pause);
+    }
+
+    static onSwipe(gesture) {
+        if (!this.pause) {
+            var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+            //Classify as right-left
+            var swipeDirection;
+            if (isHorizontal) {
+                if (gesture.direction[0] > 0) {
+                    console.log('right swipe');
+                    swipeDirection = "right";
+                    robotjs.keyTap('right');
+                    // this.setPause();
+                    // setTimeout(this.unsetPause(), 2000);
+                } else {
+                    console.log('left swipe');
+                    swipeDirection = "left";
+                    robotjs.keyTap('left');
+                }
+                // this.setPause();
+                // setTimeout(this.unsetPause(), 2000);
+                // if (swipeDirection === "right") {
+                //     robotjs.keyTap('right');
+                // } else if (swipeDirection === 'left') {
+                //     robotjs.keyTap('left');
+                // }
+            }
+            return 0;
+        }
+    }
+
+    // onTap(gesture) {
+    //     // const position = gesture.position;
+    //     // const xpos = position[0];
+    //     // const ypos = position[1];
+    //     robotjs.mouseClick();
+    //     return 0;
+    // }
+
+
+    // cursorMove(frame) {
+    //     //     var finger = frame.hands[0].fingers[0];
+    //     //
+    //     //     var mouse = robot.getMousePos();
+    //     //     robotjs.moveMouseSmooth(mouse.x + xPosLeap, mouse.y + yPosLeap);
+    //     //     return 0;
+    // }
 }
 
-function onTap(gesture) {
-  console.log("tap worked");
-  robotjs.mouseClick();
-  return 0;
-}
-
-
-function cursorMove(frame) {
-//     var finger = frame.hands[0].fingers[0];
-//
-//     var mouse = robot.getMousePos();
-//     robotjs.moveMouseSmooth(mouse.x + xPosLeap, mouse.y + yPosLeap);
-//     return 0;
-}
-
-
-var leapConnect = function(){
-  var controller = new leapjs.Controller({enableGestures: true});
-  controller.connect();
-  controller.setBackground(true);
-  return controller;
-};
-
-
-var start = function(controller) {
-  // console.log("started");
-  if(controller.connected){
-  //   console.log("connected");
-    controller.on("frame",function(frame){
-      console.log("gesture running");
+const leapConnect = function() {
+    var controller = new leapjs.Controller({
+        enableGestures: true
     });
-  }
-};
-  //     console.log(frame);
-  //     var gestureList = frame.gestures;
-  //     switch (gestureList.type){
-  //       case "keyTap":
-  //         console.log("Key Tap Gesture");
-  //         break;
-  //
-  //       case "swipe":{
-  //         // onSwipe(gesture);
-  //         if (frame.hands.length === 1)
-  //         {
-  //           // Get the hand
-  //           hand = frame.hands[0];
-  //
-  //           // Check if the hand has any fingers
-  //           fingers = hand.fingers;
-  //
-  //           gestureList = frame.gestures;
-  //
-  //           if (fingers.length >= 4 && gestureList.length > 0)
-  //           {
-  //               allSwipe = true;
-  //
-  //               for (var gesture in gestureList){
-  //                   if (gesture.type != Gesture.GestureType.TYPESWIPE || gesture.State != Gesture.GestureState.STATESTART)
-  //                   {
-  //                       allSwipe = false;
-  //                       break;
-  //                   }
-  //               }
-  //
-  //               if (!allSwipe)
-  //                   return;
-  //
-  //               console.log("recognized");
-  //              // do something
-  //           }
-  //       }
-  //         break;
-        // }
-
-  // };
-  // );
-
-    // controller.on("frame",function(frame){
-    //   // console.log("frame running");
-    //   cursorMove(frame);
-    // });
-  // }
-// };
-
-var leapDisconnect = function(controller){
-  controller.disconnect();
-  return 0;
+    controller.connect();
+    controller.setBackground(true);
+    return controller;
 };
 
 
+const start = function(controller) {
+    if (controller.connected) {
+        let play = true;
+        controller.on("gesture", function(gesture) {
+            if (play) {
+                switch (gesture.type) {
+                    case "onKeyTap":
+                        leapControl.onTap(gesture);
+                        break;
 
-var controller = leapConnect();
-console.log(controller.connected);
+                    case "swipe":
+                        // console.log('inside swipe');
+                        play = false;
+                        leapControl.onSwipe(gesture);
+                        setTimeout(() => {
+                            play = true;
+                            // console.log('inside timeout');
+                        }, 300);
+                        break;
+                }
+            }
+        });
+
+        // controller.on("frame", function(frame) {
+        //     // leapControl.cursorMove(frame);
+        // });
+    }
+};
+
+const leapDisconnect = function(controller) {
+    controller.disconnect();
+};
+
+let controller = leapConnect();
 start(controller);
-console.log("is start over ?, it shouldnt be");
